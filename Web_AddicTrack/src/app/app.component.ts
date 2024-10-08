d30d5461f5aad530dc5fc3196c84c0443e51debb
roroduJUDO <rorodujudo@gmail.com>
2024-10-08 16:33:41 +0200

    frontend sidebar

--- Web_AddicTrack/src/app/app.component.ts
+++ Web_AddicTrack/src/app/app.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+// import { CommonModule } from '@angular/common';
+// import { RouterLink } from '@angular/router';
+// import { RouterLinkActive } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
-import { RouterLinkActive } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, HeaderComponent, MatButtonModule, MatSidenavModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'AddicTrack';
+  showFiller = false;
 }
---
+++ Web_AddicTrack/src/app/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+// import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+// import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule, RouterLink, RouterLinkActive],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+}
